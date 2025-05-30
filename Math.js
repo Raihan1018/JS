85c04a8e9033af6f36bb5d03c9e4ab772335d8be
Raihan <raihanuddinahmed10@gmail.com>
2025-05-30 16:19:13 +0600

    Math operation

---
+++ Math.js
@@ -0,0 +1,52 @@
+// Math Operation
+
+//  1.Math.round() -> to make a floating number to a nearest integer
+// if the number is >= 0.5 -> convert to nearest upper number or lower number
+// let num1 = 2.99;
+// console.log(Math.round(num1));
+
+
+// Math.ceil() -> convert upper 0.5 value
+// let num2 = 4.2;
+// console.log(Math.ceil(num2))
+
+// Math.floor() -> convert lower 0.5 value
+// const num3 = 4.4
+// console.log(Math.floor(num3))
+
+
+/**
+ * random number generator 
+ * 1. generate OTP
+ * 2. user unique ID
+ * ----
+ * by default it generate numbers between 0 to 1
+ * 
+ */
+
+
+
+// let randomNumber2 = Math.floor(Math.random() * 10 + 1) // between 0 to 10
+// here 10 means upper range, random number will be 0 to 10 (upper range). If we want to generate from 1 then we will add extra +1, it means generated number +1 => that means we will never get 0
+// console.log(randomNumber2)
+
+
+// Math.max() -> return the largest number
+/**
+ * use
+ * 1. highest number
+ * 2. quiz app
+ * 3. temperature
+ */
+const x = 112;
+const y = 13;
+const z = 12;
+const maxNumber = Math.max(x,y,z)
+// console.log(maxNumber)
+// console.log(`Max number is ${Math.max(x,y,z)}`)
+
+// operation in with an array
+const numbers = [2,3,53,3,4,23,33]
+console.log(`Largest Number is: ${Math.max(...numbers)} `) // ... -> spread operator. it's copy the array
+
+console.log(`Lowest number is" ${Math.min(...numbers)}`)
---
+++ string.js
@@ -0,0 +1,16 @@
+/**
+ * 1. length
+ * 2. upper-case
+ * 3. lower-case
+ * 
+ * 
+ */
+
+// const paragraph = "Elon Musk is a South African-born American entrepreneur known for his ventures in technology and space. He co-founded PayPal and founded SpaceX, a company focused on space exploration. Musk also invested in and became CEO of Tesla, an electric car company, and recently purchased Twitter, which he renamed X. He is currently one of the wealthiest individuals in the world, with a net worth estimated at over $400 billion."
+
+// console.log("The length of the paragraph is: ", paragraph.length)
+
+
+let greeting = "Hello Developers";
+console.log(greeting.toUpperCase())
+console.log(greeting.toLowerCase())
